c6743fbf5686727c95521db7cf7e004c38526157
Marat Khambelov <mak@kodix.ru>
2020-04-23 20:51:22 +0300

    Updates

---
+++ precache-manifest.cf109d6bc764ae02e7be52c9ca922b20.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0f02c4d8efdd28281e1e276d0281f9",
+    "url": "/SkodaLanding/index.html"
+  },
+  {
+    "revision": "be8fe43835bad89d6ded",
+    "url": "/SkodaLanding/static/css/main.16a9a3e5.chunk.css"
+  },
+  {
+    "revision": "969c9c649c886161be65",
+    "url": "/SkodaLanding/static/js/2.0e7151c8.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/SkodaLanding/static/js/2.0e7151c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be8fe43835bad89d6ded",
+    "url": "/SkodaLanding/static/js/main.723323e4.chunk.js"
+  },
+  {
+    "revision": "a270ca27da9d2d3a3c73",
+    "url": "/SkodaLanding/static/js/runtime-main.efab60aa.js"
+  },
+  {
+    "revision": "ac5047c710f5e29bff4b676f049db190",
+    "url": "/SkodaLanding/static/media/SKODANext-Black.ac5047c7.woff"
+  },
+  {
+    "revision": "11ab7e356685ad3fda8d18f55d7bff2c",
+    "url": "/SkodaLanding/static/media/SKODANext-Regular.11ab7e35.woff"
+  },
+  {
+    "revision": "681242b480b7f6a86fd089e81c48ed31",
+    "url": "/SkodaLanding/static/media/banner-image-cutted.681242b4.png"
+  },
+  {
+    "revision": "39335aac0d87275b36ffbeaab80249d9",
+    "url": "/SkodaLanding/static/media/banner-image.39335aac.png"
+  },
+  {
+    "revision": "7f1f3fb8a0942b56d27818fc5d430e62",
+    "url": "/SkodaLanding/static/media/download-dark.7f1f3fb8.svg"
+  },
+  {
+    "revision": "0b35f903b02d586795198f2ca3ba6ed6",
+    "url": "/SkodaLanding/static/media/download-white.0b35f903.svg"
+  },
+  {
+    "revision": "4c189a8950dfac4fd9da865b5eb9e801",
+    "url": "/SkodaLanding/static/media/question-circle-dark.4c189a89.svg"
+  },
+  {
+    "revision": "f923d42ff8636d927ec210faf501db2b",
+    "url": "/SkodaLanding/static/media/question-circle.f923d42f.svg"
+  }
+]);
